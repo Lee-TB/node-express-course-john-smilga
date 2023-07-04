82d2570356d7beea34cbc84a97eb78575f2813f7
Lee TB <tblyduc2412@gmail.com>
2023-07-04 20:58:20 +0700

    Ongoing: 06-jobs-api

---
+++ 06-jobs-api/starter/controllers/auth.js
@@ -0,0 +1,43 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
+
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.create({ name, email, password });
+
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      name: user.name,
+    },
+    token,
+  });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password');
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError('email not exist');
+  }
+
+  const isMatch = user.comparePassword(password);
+  if (!isMatch) {
+    throw new UnauthenticatedError('wrong password');
+  }
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
+};
+
+module.exports = {
+  register,
+  login,
+};
--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -1,10 +1,32 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
-const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+const { StatusCodes } = require('http-status-codes');
 
-module.exports = errorHandlerMiddleware
+const errorHandlerMiddleware = (err, req, res, next) => {
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: err.message || 'Something went wrong try again later',
+  };
+
+  if (err.name && err.name === 'ValidationError') {
+    customError.message = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(', ');
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (err.name && err.name === 'CastError') {
+    customError.message = `Not found item with id: ${err.value}`;
+    customError.statusCode = StatusCodes.NOT_FOUND;
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.message = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  // return res.status(customError.statusCode).json(err);
+  return res.status(customError.statusCode).json({ msg: customError.message });
+};
+
+module.exports = errorHandlerMiddleware;
